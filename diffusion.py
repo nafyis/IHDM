a14826a2db0fc1b35ec3d2890df46df420e26a1b
10166 <syf_0318@126.com>
2023-02-17 17:27:29 +0800

    heat_eq_forward_finish

--- diffusion.py
+++ diffusion.py
@@ -1,9 +1,17 @@
-from scipy.fftpack import dct, idct
 import numpy as np
+from scipy.fftpack import dct, idct
+from denoising_diffusion_pytorch import Unet
 
 
-#  forward process
 def heat_eq_forward(u, w, h, t):
+    """
+    forward process
+    :param u: input picture
+    :param w: width
+    :param h: height
+    :param t: time
+    :return: ut(state at time t)
+    """
     u_proj = dct(u, axis=0, norm='ortho')
     u_proj = dct(u_proj, axis=1, norm='ortho')
     freq_w = np.linspace(0, w - 1, w)
@@ -13,6 +21,12 @@ def heat_eq_forward(u, w, h, t):
     ut = idct(u_proj, axis=0, norm='ortho')
     ut = idct(ut, axis=1, norm='ortho')
     return ut
-
 # u = [[1, 1], [2, 2]]
 # print(heat_eq_forward(u, 2, 2, 0))
+
+
+def train():
+    """
+
+    :return:
+    """
